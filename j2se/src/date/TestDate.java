44d76b5437caff6d713aa0e4a9d7f5e46f333e3e
GH <3202648312@qq.com>
2019-05-15 22:19:06 +0800

    123

--- j2se/src/date/TestDate.java
+++ j2se/src/date/TestDate.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class TestDate {
 	public static void main(String[] args) throws Exception {
 		
-		//456
+		//123
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String format = sdf.format(new Date());
 		System.out.println(format);
